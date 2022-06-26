137886bc04d8383f90fd93cdcb2b4ee17f37caf5
tmpz23 <28760271+tmpz23@users.noreply.github.com>
2022-06-26 19:28:37 +0200

    Update gc_sda_resolver.py
    commented auto-analysis to avoid brokens sdata(2) .sbss(2) data xrefs

--- IDA GCN/gc_sda_resolver.py
+++ IDA GCN/gc_sda_resolver.py
@@ -1,7 +1,7 @@
 import idaapi
 import idc
 
-__version__ = "1.0"
+__version__ = "1.1"
 __license__ = "MIT"
 __status__ = "developpement"
 
@@ -9,9 +9,10 @@ __status__ = "developpement"
 # For IDA 6.8 - tested on a friend computer
 # Translated from http://hitmen.c02.at/files/releases/gc/ppc-sdabase-fix.idc
 # Credits to Hitmen team
+# Autoanalysis will lead to broken drefs by auto renaming of .sdata(2) .sbss(2) vars
 
 
-RE_ADDR = re.compile("^0x[a-fA-F0-9]{8}\((__SDA_BASE__|__SDA2_BASE__)\)$")
+RE_ADDR = re.compile("^0x[a-fA-F0-9]{8}\((_SDA_BASE_|_SDA2_BASE_)\)$")
 RE_OFFSET = re.compile("^-*0x[a-fA-F0-9]{1,4}\((r2|r13)\)$")
 
 
@@ -25,20 +26,21 @@ def make_sda_relative_opcode(ea, realaddr, sda_base, opreg1):
         return
     elif not RE_OFFSET.match(opnd1):
         print("Error - Unknow format at ea {0:08x}:{1}".format(ea, opnd1))
-
+        return
+    
     # add data xref (from, to, drefType = Text)
     # doing this at first break sometimes .align collapsed areas discarding new dummy name
     idc.add_dref(ea, realaddr, idc.dr_T | XREF_USER)
 
-    # get name as displayed on the screen
     name = idc.Name(realaddr)
     if name == "":
-        idaapi.set_dummy_name(idaapi.BADADDR, realaddr)
+        idaapi.set_dummy_name(ea, realaddr)
+        #idc.MakeNameEx(realaddr, "sda_{:08x}".format(realaddr), idc.SN_CHECK | idc.SN_NON_AUTO)
         name = idc.Name(realaddr)
 
     output = "{0}({1})".format(
         name if name != "" else hex(realaddr)[:-1],
-        "__SDA2_BASE__" if opreg1 == 2 else "__SDA_BASE__")
+        "_SDA2_BASE_" if opreg1 == 2 else "_SDA_BASE_")
 
     # Set manually second operand with output
     idc.OpAlt(ea, 1, output)
@@ -54,14 +56,16 @@ def make_sda_imm_opcode(ea, realaddr, sda_base):
         return
     elif not RE_OFFSET.match(opnd2):
         print("Error - Unknow format at ea {0:08x}:{1}".format(ea, opnd2))
+        return
 
     # add data xref (from, to, drefType = Text)
     # doing this at first break sometimes .align collapsed areas discarding new dummy name
     idc.add_dref(ea, realaddr, idc.dr_T | XREF_USER)
 
-    name = idc.Name(ea, realaddr)
+    name = idc.Name(realaddr)
     if name == "":
-        idaapi.set_dummy_name(idaapi.BADADDR, realaddr)
+        idaapi.set_dummy_name(ea, realaddr)
+        #idc.MakeNameEx(realaddr, "sda_{:08x}".format(realaddr), idc.SN_CHECK | idc.SN_NON_AUTO)
         name = idc.Name(realaddr)
 
     # Set manually third operand with output
@@ -107,19 +111,20 @@ def resolve_sda():
     print("Waiting end of autoanalysis")
     idc.Wait()
 
+    print("Starting SDA resolver.")
     start_ea = idc.NextFunction(0)
     current_ea = start_ea
     end_ea = 0x81700000
 
-    __SDA_BASE__  = idaapi.get_segm_by_name(".sdata").startEA
-    __SDA2_BASE__ = idaapi.get_segm_by_name(".sdata2").startEA
+    _SDA_BASE_  = idaapi.get_segm_by_name(".sdata").startEA + 0x8000
+    _SDA2_BASE_ = idaapi.get_segm_by_name(".sdata2").startEA + 0x8000
 
     while current_ea != idaapi.BADADDR:
-        do_function(current_ea, __SDA_BASE__, __SDA2_BASE__)
+        do_function(current_ea, _SDA_BASE_, _SDA2_BASE_)
         current_ea = idc.NextFunction(current_ea)
 
-    print("Start analysis.")
-    idc.AnalyzeArea(start_ea, end_ea)
+    #print("Start analysis.")
+    #idc.AnalyzeArea(start_ea, end_ea)
 
     print("All done.")
 
